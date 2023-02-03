77702f3b062bac05d6684c65606bba61a2db378b
usahai90 <udbhav@mintable.com>
2023-02-04 00:30:02 +0800

    [ADD] initial commits

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1440px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
